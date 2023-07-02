da47b6225e9a0cbc15d689cbfa731bc1d5379688
Dibyanshu502 <99415170+Dibyanshu502@users.noreply.github.com>
2023-07-03 01:41:00 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,35 @@
+const Orders = [
+    {
+        productName:"Foldable Mini Drone",
+        productNumber:"85631",
+        paymentStatus:"Due",
+        shipping:"Pending"
+    },
+    
+    {
+        productName:"Larviender Drone",
+        productNumber:"35631",
+        paymentStatus:"Refunded",
+        shipping:"declined"
+    },
+    
+    {
+        productName:"Foldable Mini Drone",
+        productNumber:"82361",
+        paymentStatus:"Paid",
+        shipping:"Delivered"
+    },
+    {
+        productName:"Foldable Drone",
+        productNumber:"82361",
+        paymentStatus:"Paid",
+        shipping:"Delivered"
+    },
+    {
+        productName:"Mini Drone",
+        productNumber:"82361",
+        paymentStatus:"Paid",
+        shipping:"Delivered"
+    },
+    
+]
